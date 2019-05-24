aaaa3be19784e63262c33f865bc3d3266841e076
utsumi <30435747+S-Isozaki@users.noreply.github.com>
2019-05-24 22:00:48 +0900

    Add files via upload

--- a.vb
+++ a.vb
@@ -1,40 +1,47 @@
 Sub foo()
     Dim buf As String, cnt As Long
     Dim numberedColumn As Long
-    numberedColumn = 0
-    buf = Dir("./*chr.txt")
+    numberedColumn = 3
+    Dim Path As String
+    Path = "C:\Users\cmm_user\Desktop\“ü—ÍƒeƒXƒg\"
+    buf = Dir(Path & "*chr.txt")
     cnt = 0
     Do While buf <> ""
-        cnt = cnt + 1
-        Open buf for Input As #1
+        Open Path & buf For Input As #1
         Do
             Dim buf2 As String
             Line Input #1, buf2
-            If buf2 = "END" Then Exit Do End If
+            If buf2 = "END" Then Exit Do
             Dim stringArray() As String
-            stringArray = Split(buf2, "\t")
+            stringArray = Split(buf2, vbTab)
             Dim numberOfResultAsString As String
-            numberOfResultAsString = Left(stringArray(4), 2)
-            If isNumeric(numberOfResult) = False Then GoTo CONTINUE End If
+            numberOfResultAsString = Left(stringArray(2), 2)
+            If IsNumeric(numberOfResultAsString) = False Then GoTo CONTINUE
+            Dim Result As Double
+            Result = Val(stringArray(5))
             Dim numberOfResult As Long
             numberOfResult = Val(numberOfResultAsString)
-            Dim result As Double
-            result = Val(stringArray(6))
             Dim i As Long
-            For i = 0 To 1
-                Worksheets(i).select
+            For i = 1 To 2
+                Worksheets(i).Select
                 Dim j As Long
-                For j = 0 To 50
+                For j = 2 To 100
                     Dim s As String
-                    s = Cells(numberedColumn, j).Value
-                    If isNumeric(s) = False Then GoTo CONTINUE2 End If
+                    s = Worksheets(i).Cells(j, numberedColumn).Value
+                    If IsNumeric(s) = False Then GoTo CONTINUE2
                     Dim v As Long
                     v = Val(s)
+                    If Worksheets(i).Cells(j, numberedColumn + cnt + 9).Borders(xlDiagonalUp).LineStyle = xlContinuous Then GoTo CONTINUE2
                     If numberOfResult = v Then
-                        Worksheets(i).Cells(numberedColumn, j).Value = result
+                        Worksheets(i).Cells(j, numberedColumn + cnt + 9).Value = WorksheetFunction.Round(Result, 4)
                     End If
-                    CONTINUE2:
-            CONTINUE:
+CONTINUE2:
+                Next j
+            Next i
+CONTINUE:
         Loop
-    End Do
+        Close #1
+        cnt = cnt + 1
+        buf = Dir()
+    Loop
 End Sub
